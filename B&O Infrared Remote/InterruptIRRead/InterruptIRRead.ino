45bfdea1e9828fa5a8f63d23c3abbf0eaa9b708a
evanderknaap <eric@vdrknaap.nl>
2017-05-29 23:00:18 +0200

    cleaned up a bit

--- B&O Infrared Remote/InterruptIRRead/InterruptIRRead.ino
+++ B&O Infrared Remote/InterruptIRRead/InterruptIRRead.ino
@@ -3,20 +3,20 @@
 //#define TICK 625
 
 // This code creates a timer at 40kHz on pin 13. It checks if the PIN is high and low. 
-// On Pin 13 an IR receiver is attached, to listen to B&O commands using IR signals 
-// Digital pin 13, is B5 register
+// On Pin 10 an IR receiver is attached, to listen to B&O commands using IR signals 
+// Digital pin 10, is B5 register
 
 // Defining the Bang & Olufsen commands
-#define TICK 25 // 25 micro seconds 
+#define TICK 125 // 125 micro seconds 
 
-#define BEO_ZERO    (3125 / TICK)
-#define BEO_SAME    (6250 / TICK)
-#define BEO_ONE     (9375 / TICK)
-#define BEO_STOP    (12500 / TICK)
-#define BEO_START   (15625 / TICK)
+#define BEO_ZERO    3125
+#define BEO_SAME    6250
+#define BEO_ONE     9375
+#define BEO_STOP    12500 
+#define BEO_START   15625
 
-int counter  = 0;
 int lastState = 0;
+int index;
 int ticker  = 0;
 double freq =  1000000/TICK;
 int prescale = 16000000/(8*freq)-1;
@@ -29,10 +29,6 @@ void setup(){
     ; // wait for serial port to connect. Needed for native USB
   }
   
-  Serial.print("freq: ");
-  Serial.println(freq);
-  Serial.print("Prescale: ");
-  Serial.println(prescale);
   
   DDRB = DDRB & B11111011;   // Set Pin 10 as input, leave the rest of the pins as - is
   PORTB = DDRB & B11111011; // Set Pin 10 to LOW, leave the rest of the registers untouched
@@ -53,32 +49,43 @@ void setup(){
     TIMSK1 |= (1 << OCIE1A);
   
   sei();//allow interrupts
+  
   delay(5);
   Serial.println("setup done");
 
 }
 
-ISR(TIMER1_COMPA_vect){ // it should have a timer 
-  decode();
+ISR(TIMER1_COMPA_vect)
+{
+  processTick();
 }
 
-void decode()
+void processTick()
 {
-  counter++;
+  ticker++;
   
   int currentState = PINB; // Read the states of pin 8 - 13
   currentState = currentState & B00000100; // Read out the state of PIN 10 only
-
-  if (currentState != lastState)
-  { 
+  if(currentState != lastState)
+  {
     if(currentState - lastState < 0)
     {
-      Serial.println(counter * TICK);
-      counter = 0; 
+      Serial.println(ticker * TICK);
+      ticker = 0; 
     }
+  
     lastState =  currentState;
   }
 }
+
+int getBeoBitFromPulse(int pulse)
+{
+  int beobit = 0;
+  
+  return beobit;
+}
+
+
 
 void loop(){
 
