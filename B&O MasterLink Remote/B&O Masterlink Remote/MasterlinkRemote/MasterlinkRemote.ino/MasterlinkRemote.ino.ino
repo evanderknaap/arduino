f5d7181b9951a676b873fc56e1eddc69bc071f06
evanderknaap <eric@vdrknaap.nl>
2017-06-15 21:06:02 +0200

    created a version which seems more stable due to fluhsing the software serial buffer, turning on is excluded. Also, HDMI1 - 3 can be chosen on the beosound numpad when the tv is on

--- B&O MasterLink Remote/B&O Masterlink Remote/MasterlinkRemote/MasterlinkRemote.ino/MasterlinkRemote.ino.ino
+++ B&O MasterLink Remote/B&O Masterlink Remote/MasterlinkRemote/MasterlinkRemote.ino/MasterlinkRemote.ino.ino
@@ -3,17 +3,22 @@
 #include <EthernetUdp.h>  
 #include <SoftwareSerial.h>       
 
-#define TV_POWER 1
 #define TV_ON 1
 #define TV_OFF 0
 #define TV_OFFLINE 2
-#define TV_ERROR -1
 #define TV_TCP_PORT 20060
 #define LAST_BYTE 23
-#define TIMEOUT_TIME 3000 // If we did not receive a reponse after 3[sec] from the tv, we concluded it failed & we close the connection
+#define TIMEOUT_TIME 4000 // If we did not receive a reponse after 3[sec] from the tv, we concluded it failed & we close the connection
 #define RX_PORT 9
 #define TX_PORT 10
-#define beo_cmd_standby 17
+#define beo_cmd_standby 0x11
+#define beo_master_audio 0xC1
+#define beo_HMDI1_1 0xAB
+#define beo_HMDI1_2 0xDD
+#define beo_HMDI2_1 0xAC
+#define beo_HMDI2_2 0xDE
+#define beo_HMDI3_1 0xAD
+#define beo_HMDI3_2 0xDF
 
 IPAddress arduino_ip(192, 168, 178, 10); 
 IPAddress tv_ip(192,168,178,31);  
@@ -22,11 +27,18 @@ byte tvMAC[] = {0xC0,0x38,0x96,0x67,0x1D,0xA9};
 byte abyTargetIPAddress[] = { 192, 168, 178, 255 };                                                          // Broadcast IP for Wake On Lan       
 EthernetClient client;                          
 EthernetUDP Udp;
-SoftwareSerial mySerial(9, 8); 
+SoftwareSerial mySerial(RX_PORT, TX_PORT); 
 
-byte snd_read[24] = {42,83,69,80,79,87,82,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,10};                     // read tv power state command
-byte snd_on[24] = {42,83,67,80,79,87,82,'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1',10};       // switch power off command
-byte snd_off[24] = {42,83,67,80,79,87,82,'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0',10};      // switch power off command
+//byte snd_read[24] = {42,83,69,80,79,87,82,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,35,10};                     // read tv power state command
+//byte snd_on[24] = {42,83,67,80,79,87,82,'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','1',10};       // switch power off command
+//byte snd_off[24] = {42,83,67,80,79,87,82,'0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0',10};      // switch power off command
+const unsigned char snd_off[25] = "*SCPOWR0000000000000000\n";
+const unsigned char snd_on[25] = "*SCPOWR0000000000000001\n";
+const unsigned char snd_read[25] = "*SEPOWR################\n";
+const unsigned char snd_HDMI1[25] = "*SCINPT0000000100000001\n";
+const unsigned char snd_HDMI2[25] = "*SCINPT0000000100000002\n";
+const unsigned char snd_HDMI3[25] = "*SCINPT0000000100000003\n";
+const unsigned char snd_error[3] = "-1";
 
 byte response[24];
 byte beo_command[100];
@@ -35,7 +47,7 @@ int counter = 0;
 void setup() {
   
   Ethernet.begin(arduino_mac, arduino_ip);  // Initalize the Ethernet board 
-  //Serial.begin(9600);       // Uncomment for debugging
+ // Serial.begin(9600);       // Uncomment for debugging
   mySerial.begin(19200);
   delay(1000);
   Serial.println("setup complete");  
@@ -48,8 +60,9 @@ void loop() {
     delay(10); 
     int c =  mySerial.read();
     beo_command[counter] = c;
-    Serial.print(c,HEX); // For debugging
+    Serial.println(c,HEX); // For debugging
     counter++;
+    if(mySerial.overflow()) Serial.println("overflow");
   }
 
   // New command received
@@ -58,24 +71,29 @@ void loop() {
     Serial.println();
     counter = 0;
 
-    Serial.println("Connecting"); 
-    if(!client.connect(tv_ip,TV_TCP_PORT)) // Does not matter the key, if the tv is offline - WakeOnLan 
-    {
-      Serial.println("TV not online, waking on lan");
-      wakeTVOnLan();
-    }
-    else
+   const unsigned char * cmd = didReceiveStandbyCommand(beo_command);
+   Serial.print("size off ");
+   Serial.println(sizeof(cmd));
+   Serial.println(cmd[0]);
+   if(cmd[0] == '-') return; // do nothing if we didn't receive a standby command
+
+    Serial.println("Check connection"); 
+    if(client.connect(tv_ip,TV_TCP_PORT)) // Does not matter the key, if the tv is offline - WakeOnLan 
     {
       Serial.println("Connected");
       int state = getTVPowerState();
-      if(client.connect(tv_ip, TV_TCP_PORT)) sendBeoCommandFromBytesAndTvState(beo_command, state);
+      if(client.connect(tv_ip, TV_TCP_PORT) && state == TV_ON) sendMessage(cmd);
       else Serial.println("connection failed");
+    }
+    else
+    {
+      Serial.println("TV not online, do nothing");
     }
   }
   
 }
 
-void sendMessage( byte message [])
+void sendMessage( const unsigned char message [])
 {
   if (!client.connected()) return;
 
@@ -128,47 +146,53 @@ int getTVPowerState()
     else return TV_OFFLINE;
 }
 
-void wakeTVOnLan()
+const unsigned char * didReceiveStandbyCommand(byte cmd [])
 {
-  const int nMagicPacketLength = 102;
-  byte abyMagicPacket[nMagicPacketLength] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-  byte abyTargetIPAddress[] = { 192, 168, 178, 255 }; // broadcast
-    
-  // Compose magic packet to wake remote machine. 
-  for (int ix = 6; ix < 102; ix++) {
-    abyMagicPacket[ix] = tvMAC[ix % 6];
+  if(cmd[7] == beo_cmd_standby && cmd[1] == beo_master_audio)
+  {
+    Serial.println("its mater audio telling to shut down");
+    return snd_off;
   }
-  
-  Udp.begin(7);
-  if (Udp.beginPacket(abyTargetIPAddress, 7)) {
-    Serial.println("sending package");
-    Udp.write(abyMagicPacket, nMagicPacketLength);
-    Udp.endPacket();
+  else if (cmd[1] == beo_master_audio && cmd[11] == beo_HMDI1_1 && cmd[12] == beo_HMDI1_2)
+  {
+    Serial.println("Its master audio telling HDMI 1");
+    return snd_HDMI1;
   }
-
-  Udp.stop();
+  else if (cmd[1] == beo_master_audio && cmd[11] == beo_HMDI2_1 && cmd[12] == beo_HMDI2_2)
+  {
+    Serial.println("Its master audio telling HDMI2");
+    return snd_HDMI2;
+  }
+   else if (cmd[1] == beo_master_audio && cmd[11] == beo_HMDI3_1 && cmd[12] == beo_HMDI3_2)
+  {
+     Serial.println("Its master audio telling HDMI3");
+    return snd_HDMI3;
+  }
+  else
+  {
+    return snd_error;
+  }
 }
 
-void sendBeoCommandFromBytesAndTvState(byte cmd [], int state)
+
+void sendBeoCommandFromBytesAndTvState(const unsigned char * cmd, int state)
 {  
   Serial.print(" The 8th bit: ");
   Serial.println(cmd[7]);
   
-  if(cmd[7] == beo_cmd_standby)
-  { 
-    if(state == TV_ON) 
-    {
-      Serial.println("Switch tv off");
-      sendMessage(snd_off);
-    }
-    else if(state == TV_OFF)
-    {
-      Serial.println("Switch tv on");
-      sendMessage(snd_on);
-    }
-    else;
-  }  
+  if(state == TV_ON) 
+  {
+    Serial.println("Switch tv off");
+    sendMessage(snd_off);
+  }
+  else if(state == TV_OFF)
+  {
+    Serial.println("Switch tv on");
+    sendMessage(snd_on);
+  }
 }
+
+
 
 
 
